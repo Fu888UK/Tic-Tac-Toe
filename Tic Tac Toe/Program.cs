f88ba4ab8971ea78b92e0c691fe3420436c39716
Fu888UK <fu83man@hotmail.co.uk>
2025-07-11 12:08:58 +0100

    logic file removed

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -9,3 +9,10 @@
         }
     }
 }
+
+
+//3x3 grid 
+//set player one to X and player 2 to O
+//take turns choosing symbol in empty space on the grid 
+//first one to 3 in a row wins 
+//if no player wins its a draw 
