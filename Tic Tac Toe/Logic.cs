4fa3e4ca139bdabd611a48dddb71622548125f70
Fu888UK <fu83man@hotmail.co.uk>
2025-07-11 13:08:00 +0100

    made a start with new project created files for logic and UI started writing code for UI file (Write and Read Lines) started writing code for Logic where I created some variables that I think I will need, and created and array for the grid next step to create the grid, hava commented out below the code that was written for the 2D arrays project (Mode 3) that I think will help me to create the grid

---
+++ Tic Tac Toe/Logic.cs
@@ -0,0 +1,71 @@
+﻿namespace Tic_Tac_Toe
+{
+    internal class Logic
+    {
+        static void Main(string[] args)
+        {
+            const int ROWS = 3;
+            const int COLS = 3;
+            const char SYMBOL1 = 'O';
+            const char SYMBOL2 = 'X';
+            
+            
+            Random rnd = new Random();              //array
+            int[,] grid = new int[ROWS,COLS];
+
+            for (int i = 0; i < ROWS; i++) 
+            {
+                for (int j = 0; j < COLS; j++) 
+                {
+                    //refer to 2D arrays project (Mode 3)
+                    //Console.WriteLine(grid[i,j] + " ");
+                }
+            }
+
+        }
+    }
+}
+
+
+//3x3 grid 
+//set player one to X and computer to O or visa versa, let the player choose which symbol they want 
+//take turns choosing symbol in empty space on the grid 
+//first one to 3 in a row wins 
+//if no player wins its a draw 
+
+
+
+//TAKEN FROM 2d ARRAYS PROJECT 
+//for (int i = 0; i < rows; i++)                                //outer loop i goes through each row 
+//{
+//    for (int j = 0; j < cols; j++)                            // inner loop j goes through eacj col 
+//    {
+//        grid[i, j] = ((i + j) % 2 == 0) ? SYM1 : SYM2;        //checks if i and j are even 
+//    }
+//}
+//Console.Write("+");
+
+//for (int j = 0; j < cols; j++)              //top border 
+//{
+//    Console.Write("-+");                   //??? added a + but now getting 2 extra -                   
+//                                           //Console.Write("+");                   //??? AFTER DUBUGGING FEEL I SHOULD ADD SOMETHING HERE (FOR COL)                     
+//}
+//Console.WriteLine("");
+
+//for (int i = 0; i < rows; i++)
+//{
+//    Console.Write("|");
+//    for (int j = 0; j < cols; j++)
+//    {
+//        Console.Write(grid[i, j] + "|");    //replaced space with|, but now has extra | at the end 
+//    }
+//    Console.WriteLine("");
+
+//}
+
+//Console.Write("+");
+//for (int j = 0; j < cols; j++)  //bottom border 
+//{
+//    Console.Write("-+");       //added a +
+//}
+//Console.WriteLine("");
---
+++ Tic Tac Toe/UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public static class UI_Method
+    {
+        public static void DisplayWelcomeMessage() 
+        {
+            Console.WriteLine("Hello, welcome to the world of TIC TAC TOE");
+            Console.WriteLine("This a game where you take turns against the computer using a symbol X or O to fill in a 3x3 grid");
+            Console.WriteLine("The wwinner of the game is the first one to get 3 symbols in a row");
+            Console.WriteLine("Select the symbol you wish to play with, press 1 for X or 2 for O");
+
+            string userSelection = Console.ReadLine();
+
+            Console.WriteLine($"You have chosen to play with {userSelection} ");
+
+        }
+        
+
+    }
+}
